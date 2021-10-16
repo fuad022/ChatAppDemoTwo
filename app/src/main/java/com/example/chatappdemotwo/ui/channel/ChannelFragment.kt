09d3c5709c0e611147e3df5f764c8544ed09b52f
f.r.malikov <fuad.malikov@rabitabank.com>
2021-10-17 00:21:58 +0400

    Some changes.

--- app/src/main/java/com/example/chatappdemotwo/ui/channel/ChannelFragment.kt
+++ app/src/main/java/com/example/chatappdemotwo/ui/channel/ChannelFragment.kt
@@ -1,14 +1,11 @@
 package com.example.chatappdemotwo.ui.channel
 
 import android.os.Bundle
-import android.util.Log
 import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.activityViewModels
+import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.chatappdemotwo.R
@@ -16,40 +13,38 @@ import com.example.chatappdemotwo.adapter.UserHorizontalAdapter
 import com.example.chatappdemotwo.adapter.UserVerticalAdapter
 import com.example.chatappdemotwo.databinding.FragmentChannelBinding
 import com.example.chatappdemotwo.model.UserModel
-import java.lang.Exception
-import java.nio.channels.Selector
-
-import java.text.SimpleDateFormat
-import java.util.*
-import kotlin.collections.ArrayList
 
 class ChannelFragment : Fragment() {
     private val binding by lazy { FragmentChannelBinding.inflate(layoutInflater) }
-    private val imageList = listOf(
-        R.drawable.photo_1,
-        R.drawable.photo_2,
-        R.drawable.photo_3,
-        R.drawable.photo_4,
-        R.drawable.photo_5,
-        R.drawable.photo_6,
-        R.drawable.photo_7
-    )
-    private var verticalUsersList = ArrayList<UserModel>()
-
     private val viewModel: UserViewModel by viewModels()
     private val userHorizontalAdapter = UserHorizontalAdapter()
     private val userVerticalAdapter = UserVerticalAdapter()
+    private val usersList = arrayListOf<UserModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
         initToolbar()
+        doSearch()
         setupHorizontalAdapter()
         setupVerticalAdapter()
-
         return binding.root
+    }
+
+    private fun doSearch() {
+        binding.search.doAfterTextChanged {
+            filter(it.toString())
+        }
+    }
+
+    private fun filter(text: String) {
+        val filteredUsersList = arrayListOf<UserModel>()
+        usersList.forEach {
+            if (text.lowercase() in it.friendUsername.toString().lowercase())
+                filteredUsersList.add(it)
+            userVerticalAdapter.submitList(filteredUsersList.toMutableList())
+        }
     }
 
     private fun initToolbar() {
@@ -93,6 +88,7 @@ class ChannelFragment : Fragment() {
 
     private fun observeVerticalList() {
         viewModel.mockUserList.observe(viewLifecycleOwner, {
+            usersList.addAll(it)
             userVerticalAdapter.submitList(it.toMutableList())
             binding.usersRecyclerViewVertical.layoutManager = LinearLayoutManager(activity)
             binding.usersRecyclerViewVertical.adapter = userVerticalAdapter
